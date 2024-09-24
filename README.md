9e0a4c3b218b563c9675ca1562475fa70205c153
gottiee <gottiedev@gmail.com>
2024-09-24 10:28:43 -0400

    debut de loop un petit peu fonctionnel

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 non-Raw socket means you can just determine Transport Layer Payload. i.e it is the OS' task to create the Transport, Network, and Data Link layer headers.
 
 Raw socket means you can determine every section of a packet, be it header or payload. Please note that raw socket is a general word. I categorize raw socket into: Network Socket and Data-Link Socket (or alternativly L3 Socket and L2 Socket).
+
+- [Leak bug getaddrinfo](https://bugs.kde.org/show_bug.cgi?id=448991)
+    - [stackoverflow](https://stackoverflow.com/questions/77642568/valgrind-showing-still-reachable-memory-leak-with-getaddrinfo)
+    - [another stackoverflow](https://stackoverflow.com/questions/73976258/memory-leaks-observed-in-getgrnam-and-getgrnam-r-in-linux)
+
+```sh
+# show leak bug
+sudo valgrind --leak-check=full  --show-leak-kinds=all ./ft_ping google.com
+# suppression of leak bug
+sudo valgrind  --suppressions=dlopen.supp ./ft_ping google.com
+```
--- src/ping.c
+++ src/ping.c
@@ -1,6 +1,6 @@
 #include "../inc/ping.h"
 
-int pingloop = 1;
+int nbr_loop = 1;
 
 unsigned short checksum(void *b, int len)
 {
@@ -53,7 +53,7 @@ char *fill_sockaddr_in(struct sockaddr_in *addr_con, char *input)
     if (inet_pton(AF_INET, input, &(addr_con->sin_addr)) == 1) {
         addr_con->sin_family = AF_INET;
         addr_con->sin_port = htons(0);  // port ?
-        strncpy(ip, input, sizeof(ip) - 1);
+        strncpy(ip, input, 1024);
         return ip;
     }
     dns_lookup(input, ip, addr_con);
@@ -72,44 +72,58 @@ int socket_creation()
 
 void ping_loop(int sockfd, t_info info, struct sockaddr_in *ping_addr)
 {
-    (void)sockfd;
-    (void)info;
-    (void)ping_addr;
-    // int flag, i, msg_count = 0;
-    // char rbuffer[128];
-    // t_ping_pkt pckt;
-    // struct sockaddr_in r_addr;
+    int flag, msg_count = 0;
+    socklen_t addr_len;
+    long unsigned i;
+    char rbuffer[500];
+    t_ping_pkt pckt;
 
-    // // struct timespec time_start, time_end, tfs, tfe;
-    // // long double rtt_msec = 0, total_msec = 0;
+    (void)flag;
 
-    // struct timeval tv_out;
-    // tv_out.tv_sec = RECV_TIMEOUT;
+    struct sockaddr_in r_addr;
+
+    // struct timespec time_start, time_end, tfs, tfe;
+    // long double rtt_msec = 0, total_msec = 0;
+
+    struct timeval tv_out;
+    tv_out.tv_sec = RECV_TIMEOUT;
+    tv_out.tv_usec = 0;
     
-    // if (setsockopt(sockfd, SOL_IP, IP_TTL, &info.ttl, sizeof(info.ttl)) != 0)
-    //     fatal_perror("Error: setup ttl to socket");
-    // if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof(tv_out)) != 0)
-    //     fatal_perror("Error: setup timeout socket");
+    if (setsockopt(sockfd, SOL_IP, IP_TTL, &info.ttl, sizeof(info.ttl)) != 0)
+        fatal_perror("Error: setup ttl to socket");
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv_out, sizeof(tv_out)) != 0)
+        fatal_perror("Error: setup timeout socket");
     
-    // while (ping_loop)
-    // {
-    //     flag = 1;
-    //     bzero(&pckt, sizeof(pckt));
-    //     pckt.hdr.type = ICMP_ECHO;
-    //     pckt.hdr.un.echo.id = getpid();
-    //     for (i = 0; i < sizeof(pckt.msg) - 1; i++)
-    //         pckt.msg[i] = i + '0';
-    //     pckt.msg[i] = 0;
-    //     pckt.hdr.un.echo.sequence = msg_count++;
-    //     pckt.hdr.checksum = checksum(&pckt,sizeof(pckt));
+    while (nbr_loop)
+    {
+        flag = 1;
+        bzero(&pckt, sizeof(pckt));
+        pckt.hdr.type = ICMP_ECHO;
+        pckt.hdr.un.echo.id = getpid();
+        for (i = 0; i < sizeof(pckt.msg) - 1; i++)
+            pckt.msg[i] = i + '0';
+        pckt.msg[i] = 0;
+        pckt.hdr.un.echo.sequence = msg_count++;
+        pckt.hdr.checksum = checksum(&pckt,sizeof(pckt));
 
-    //     usleep(info.sleep_rate);
+        usleep(info.sleep_rate);
 
-    //     //send packet
-    //     if (sendto(sockfd, &pckt, sizeof(pckt), 0, (stuct sockaddr *)ping_addr, sizeof(*ping_addr)) <= 0 )
-    //         printf("\nPacket Sending Failed\n");
-    //         flag = 0;
-    // }
+        //send packet
+        if (sendto(sockfd, &pckt, sizeof(pckt), 0, (struct sockaddr *)ping_addr, sizeof(*ping_addr)) <= 0 )
+        {
+            printf("\nPacket Sending Failed\n");
+            flag = 0;
+        }
+
+        // receive packet
+        addr_len = sizeof(r_addr);
+        if (recvfrom(sockfd, rbuffer, sizeof(rbuffer), 0, (struct sockaddr *)&r_addr, &addr_len) <= 0 && msg_count > 1)
+            printf("packet receive failed\n");
+        else
+        {
+            printf("packet receive\n");
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -121,15 +135,12 @@ int main(int argc, char **argv)
     int sockfd = socket_creation();
     if (sockfd == -1)
         fatal_perror("Error: socket creation");
-    t_ping_pkt ping_pkt;
-
-    (void)ping_pkt;
 
     //modifier, je ne dois pas prendre argv1 normalement
     ip_addr = fill_sockaddr_in(&addr_con, argv[1]);
+
     t_info info = manage_args(argv);
-    (void)info;
-    // ping_loop(sockfd, info);
+    ping_loop(sockfd, info, &addr_con);
     printf("ip : %s\n", ip_addr);
     free(ip_addr);
 }
